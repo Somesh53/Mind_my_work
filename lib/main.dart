6c3046a34fa141235bcf9f9b9fb85387f3feb46b
Somesh53 <s_verma@ma.iitr.ac.in>
2022-03-23 17:13:10 +0530

    my app

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: prefer_const_constructors, deprecated_member_use
+
+import 'package:assignment/pages/home.dart';
+import 'package:assignment/pages/profile.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      // home: homePage(),
+      routes: {
+        '/': (context) => homePage(),
+        '/profile': (context) => ProfilePage(),
+      },
+    ));
